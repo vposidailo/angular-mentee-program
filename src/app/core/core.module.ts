4891567618a0018febd93a5a25273cdcbfad4253
Vitalii_Posidailo <vposidaylo@gmail.com>
2018-06-16 23:37:08 +0200

    Fixed issue with import for header.

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   imports: [
-    CommonModule,
-    HeaderComponent
-  ],
-  declarations: [HeaderComponent]
+    CommonModule
+  ]
 })
 export class CoreModule { }
