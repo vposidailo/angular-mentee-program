50560e158f40936b1e584d82e7e0f4a15342edb2
Vitalii_Posidailo <vposidaylo@gmail.com>
2018-06-17 20:43:30 +0200

    Added html for video-course component and video-course-item cimponent;

--- src/app/video-courses-list/video-courses-list-item-component/video-courses-list-item-component.component.ts
+++ src/app/video-courses-list/video-courses-list-item-component/video-courses-list-item-component.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./video-courses-list-item-component.component.css']
 })
 export class VideoCoursesListItemComponentComponent implements OnInit {
-
+ 
   constructor() { }
 
   ngOnInit() {
