4a25e7c33e80f59cb58ce4b6ba65dd384e58b0a6
Vitalii_Posidailo <vposidaylo@gmail.com>
2018-06-17 11:05:45 +0200

    Added header website name; Added footer component; Imported footer component to app component; Added CSS style for footer;

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { CoreModule } from './core/core.module';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './core/header/header.component';
-import { CoreModule } from './core/core.module';
+import { FooterComponent } from './core/footer/footer.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
