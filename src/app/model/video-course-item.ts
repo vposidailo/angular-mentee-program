c0e455a91da3d53a0f0c7379dcb3c41bc1b2aa07
Vitalii_Posidailo <vposidaylo@gmail.com>
2018-06-17 22:04:41 +0200

    Added VideoCourseItem interface; Implemented VideoCourseItem interface;

---
+++ src/app/model/video-course-item.ts
@@ -0,0 +1,7 @@
+export interface VideoCourseItem {
+    id: number,
+    Title: string,
+    Description: string,
+    Duration: number,
+    Creationdate: Date
+}
--- src/app/video-courses-list/video-courses-list-item-component/video-courses-list-item-component.component.ts
+++ src/app/video-courses-list/video-courses-list-item-component/video-courses-list-item-component.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { VideoCourseItem } from '../../model/video-course-item';
 
 @Component({
   selector: 'app-video-courses-list-item-component',
@@ -6,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./video-courses-list-item-component.component.css']
 })
 export class VideoCoursesListItemComponentComponent implements OnInit {
- 
+  @Input() public VCItem: VideoCourseItem;
+  
   constructor() { }
 
   ngOnInit() {
