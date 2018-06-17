8aab48052432d5823b1cee8430c26f19699628db
Vitalii_Posidailo <vposidaylo@gmail.com>
2018-06-17 12:25:31 +0200

    Added bootstrap; Modified header and footer styles; Added Breadcrumbs component;

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,11 @@ import { NgModule } from '@angular/core';
 import { CoreModule } from './core/core.module';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './core/header/header.component';
-import { FooterComponent } from './core/footer/footer.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    FooterComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [HeaderComponent, FooterComponent, BreadcrumbsComponent],
+  exports: [HeaderComponent, FooterComponent,]
 })
 export class CoreModule { }
