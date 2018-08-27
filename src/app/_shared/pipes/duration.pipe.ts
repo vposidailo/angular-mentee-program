34a6887eb26710f48d3a5580425fb45c77777859
BUDAPEST\vitalii_posidailo <vitalii_posidailo@epam.com>
2018-08-27 17:49:32 +0200

    Use ReactiveForm approach for duration component;

--- src/app/_shared/pipes/duration.pipe.ts
+++ src/app/_shared/pipes/duration.pipe.ts
@@ -8,6 +8,8 @@ export class DurationPipe implements PipeTransform {
   transform(value: any, args?: any): string {
     if (value < 60) {
       return value % 60 + 'min';
+    } else if (value % 60 === 0) {
+      return ((value - value % 60) / 60) + 'h';
     }
 
     return ((value - value % 60) / 60) + 'h ' + value % 60 + 'min';
--- src/app/core/controls/duration/duration.component.ts
+++ src/app/core/controls/duration/duration.component.ts
@@ -1,20 +1,31 @@
-import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { FormGroup, FormControl, RequiredValidator, Validators } from '@angular/forms';
+import { debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-duration',
   templateUrl: './duration.component.html',
   styleUrls: ['./duration.component.css']
 })
-export class DurationComponent implements OnInit {
+export class DurationComponent implements OnInit, OnChanges {
+
+  durationForm = new FormGroup({
+    duration: new FormControl('', [Validators.required, Validators.pattern('[0-9]*')] )
+  });
+
   @Input() public minutes = 0;
   @Output() public durationOutput: EventEmitter<number> = new EventEmitter<number>();
 
   constructor() { }
 
   ngOnInit() {
+    this.durationForm.get('duration').valueChanges
+    .pipe(
+      debounceTime(1000))
+    .subscribe((minutes: number) => this.durationOutput.emit(minutes));
   }
 
-  durationChanged() {
-    this.durationOutput.emit(this.minutes);
+  ngOnChanges(changes: SimpleChanges) {
+    this.durationForm.controls['duration'].setValue(changes['minutes'].currentValue);
   }
 }
